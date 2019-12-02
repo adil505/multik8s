78b157070d9d817938c225720ebe2e1adbb88571
adil <adil505@hotmail.com>
2019-12-02 17:43:20 +0000

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t astelware/multi-docker-client:latest -t astelware/multi-docker-client:$SHA ./client/Dockerfile ./client
+docker build -t astelware/multi-docker-server:latest -t astelware/multi-docker-server:$SHA ./server/Dockerfile ./server
+docker build -t astelware/multi-docker-worker:latest -t astelware/multi-docker-worker:$SHA ./worker/Dockerfile ./worker
+docker push astelware/multi-docker-client:latest
+docker push astelware/multi-docker-server:latest
+docker push astelware/multi-docker-worker:latest
+docker push astelware/multi-docker-client:$SHA
+docker push astelware/multi-docker-server:$SHA
+docker push astelware/multi-docker-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=astelware/multi-docker-client:$SHA
+kubectl set image deployments/server-deployment server=astelware/multi-docker-server:$SHA
+kubectl set image deployments/worker-deployment worker=astelware/multi-docker-worker:$SHA
